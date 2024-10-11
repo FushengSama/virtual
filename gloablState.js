879029088936454c21a63dcdbcaf953f99d36436
FushengSama <121780150+FushengSama@users.noreply.github.com>
2024-10-11 23:55:26 +0800

    globalIndex

--- gloablState.js
+++ gloablState.js
@@ -2,9 +2,13 @@
 import { reactive } from 'vue';
 
 export const globalState = reactive({
-  globalVariable: 0
+  globalVariable: 0,
+  globalIndex:""
 });
 
 export function setGlobalVariable(newValue) {
   globalState.globalVariable = newValue;
+}
+export function setGlobalIndex(newIndex){
+  globalIndex.globalIndex = newIndex;
 }
